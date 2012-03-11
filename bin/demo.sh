6bd9b60554b127ead03d0885c7dcf81754235ed9
Mario Juric <mjuric@lsst.org>
2012-03-11 14:53:47 -0700

    initial commit: Winter2012-processable processCcdLsstSim.py demo

---
+++ bin/demo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+type processCcdLsstSim.py >/dev/null 2>&1 || { echo "Could not find processCcdLsstSim.py on your path. Have you sourced loadLSST.sh, and setup-ed pipe_tasks?"; exit 1; }
+test -d input || { echo "Could not fine the 'input' directory. Run this script from the directory where the 'input' subdirectory resides."; exit 1; }
+test -d astrometry_net_data || { echo "Could not fine the 'astrometry_net_data' directory."; exit 1; }
+
+set -e
+
+# Tell the stack where to find astrometric reference catalogs
+setup -v -r ./astrometry_net_data astrometry_net_data
+
+rm -rf output detected-sources.txt
+processCcdLsstSim.py lsstSim input --id visit=886894611 raft=2,3 sensor=1,1 --out output
+./bin/export-results output > detected-sources.txt
+
+echo
+echo "Processing completed successfully. The results are in detected-sources.txt."
