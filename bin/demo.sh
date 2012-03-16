3c8c204b4a84a500d998a5cd43725b0587b8c251
Mario Juric <mjuric@lsst.org>
2012-03-15 17:33:54 -0700

    demo.sh: Make it work when user's shell is not bash

--- bin/demo.sh
+++ bin/demo.sh
@@ -3,7 +3,7 @@
 type processCcdLsstSim.py >/dev/null 2>&1 || { echo "Could not find processCcdLsstSim.py on your path. Have you sourced loadLSST.sh, and setup-ed pipe_tasks?"; exit 1; }
 test -d input || { echo "Could not fine the 'input' directory. Run this script from the directory where the 'input' subdirectory resides."; exit 1; }
 test -d astrometry_net_data || { echo "Could not fine the 'astrometry_net_data' directory."; exit 1; }
-
+test "$(type -t setup)" == "function" || { export SHELL=/bin/bash; source $LSST_HOME/eups/default/bin/setups.sh; } # Ensure 'setup' alias is defined (may happen if the user is not running bash)
 set -e
 
 # Tell the stack where to find astrometric reference catalogs
