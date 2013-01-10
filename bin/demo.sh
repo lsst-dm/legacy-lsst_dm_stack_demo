57255e6c9ee85d72150a203e45eaef16fdd7bdde
Kian-Tat Lim <ktl@slac.stanford.edu>
2013-01-10 00:06:27 -0600

    processCcdSdss moved to obs_sdss, so check that that package is setup first (and change error message).

--- bin/demo.sh
+++ bin/demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-type processCcdSdss.py >/dev/null 2>&1 || { echo "Could not find processCcdSdss.py on your path. Have you sourced loadLSST.sh, and setup-ed pipe_tasks?"; exit 1; }
 eups list --setup obs_sdss >/dev/null 2>&1 || ( echo "obs_sdss does not appear to be setup, or eups is not configured correctly."; exit 1; )
+type processCcdSdss.py >/dev/null 2>&1 || { echo "Could not find processCcdSdss.py on your path. It is supposed to be in obs_sdss."; exit 1; }
 test -d input || { echo "Could not find the 'input' directory. Run this script from the directory where the 'input' subdirectory resides."; exit 1; }
 test -d astrometry_net_data || { echo "Could not fine the 'astrometry_net_data' directory."; exit 1; }
 test "$(type -t setup)" == "function" || { export SHELL=/bin/bash; source $LSST_HOME/eups/default/bin/setups.sh; } # Ensure 'setup' alias is defined (may happen if the user is not running bash)
