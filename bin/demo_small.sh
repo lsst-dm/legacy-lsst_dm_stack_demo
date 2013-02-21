f696089f7c7006290aa830f4de05f44926b1c151
Robyn Allsman <robyn@LSST.org>
2013-02-21 16:36:09 -0600

    Ticket #2677: "demo2012 fails on VM which have <= 8GB accessible mem+swap" * Added bin/demo_small.sh to reduce filters processed so Buildbot slave VMs   will not run out of hard limit of (mem+swap) = 8GB. * Updated bin/export-results to handle variable filter set in output dataset. * Added detected-sources.txt.expected_small as comparison file for smaller run. * Updated detected-sources.txt.expected which was out-of-date wrt   master stack runs. * Update README to reflect the addition of the demo suitable for   constrained memory systems.

---
+++ bin/demo_small.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+eups list --setup obs_sdss >/dev/null 2>&1 || ( echo "obs_sdss does not appear to be setup, or eups is not configured correctly."; exit 1; )
+type processCcdSdss.py >/dev/null 2>&1 || { echo "Could not find processCcdSdss.py on your path. It is supposed to be in obs_sdss."; exit 1; }
+test -d input || { echo "Could not find the 'input' directory. Run this script from the directory where the 'input' subdirectory resides."; exit 1; }
+test -d astrometry_net_data || { echo "Could not fine the 'astrometry_net_data' directory."; exit 1; }
+test "$(type -t setup)" == "function" || { export SHELL=/bin/bash; source $LSST_HOME/eups/default/bin/setups.sh; } # Ensure 'setup' alias is defined (may happen if the user is not running bash)
+set -e
+
+# Tell the stack where to find astrometric reference catalogs
+setup --nolocks -v -r ./astrometry_net_data astrometry_net_data
+
+rm -rf output detected-sources.txt
+processCcdSdss.py input --id run=4192 filter=g^i^z camcol=4 field=300 --id run=6377 filter=u^r^i camcol=4 field=399 --output output
+./bin/export-results output > detected-sources.txt
+
+echo
+echo "Processing completed successfully. The results are in detected-sources.txt."
